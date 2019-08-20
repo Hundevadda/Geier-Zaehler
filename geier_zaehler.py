07c4d53bec501894e6464540383be6738c836c7d
Peter Schinagl <P.Schinagl@web.de>
2019-08-20 19:26:14 +0200

    Add KaddlAbend class

---
+++ geier_zaehler.py
@@ -0,0 +1,6 @@
+class KaddlAbend:
+    def __init__(self, mitspieler1, mitspieler2, mitspieler3, bilanz):
+        self.spieler1 = mitspieler1
+        self.spieler2 = mitspieler2
+        self.spieler3 = mitspieler3
+        self.bilanz = bilanz
