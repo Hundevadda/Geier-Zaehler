3b5acd40bab5449c00b6ea5eb349739ffadac21d
Peter Schinagl <P.Schinagl@web.de>
2019-08-20 19:34:35 +0200

    Add date and json print func to KaddlAbend class And add example data set

--- geier_zaehler.py
+++ geier_zaehler.py
@@ -1,6 +1,17 @@
+import json
+import datetime
+
 class KaddlAbend:
-    def __init__(self, mitspieler1, mitspieler2, mitspieler3, bilanz):
+    def __init__(self, mitspieler1, mitspieler2, mitspieler3, datum, bilanz):
         self.spieler1 = mitspieler1
         self.spieler2 = mitspieler2
         self.spieler3 = mitspieler3
         self.bilanz = bilanz
+        self.datum = str(datum)
+    def toJSON(self):
+        return json.dumps(self, default=lambda o: o.__dict__,
+            sort_keys=True, indent=4)
+
+x = KaddlAbend("Nusse", "Reissy", "Flo", datetime.date.today(), 3.50)
+print(x.toJSON())
+print(x.datum)
